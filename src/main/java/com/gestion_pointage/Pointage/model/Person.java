b4cffa50730d438aa988060cbc9be9d8f2754ad4
kh-chaiboub <k.chaiboub@outlook.com>
2022-08-25 00:09:49 +0200

    Write code for model packege

--- src/main/java/com/gestion_pointage/Pointage/model/Person.java
+++ src/main/java/com/gestion_pointage/Pointage/model/Person.java
@@ -1,4 +1,52 @@
 package com.gestion_pointage.Pointage.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String cin;
+    private String name;
+    private String lastname;
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
--- src/main/java/com/gestion_pointage/Pointage/repository/PersonRopository.java
+++ src/main/java/com/gestion_pointage/Pointage/repository/PersonRopository.java
@@ -1,4 +1,10 @@
 package com.gestion_pointage.Pointage.repository;
 
-public interface PersonRopository {
+import com.gestion_pointage.Pointage.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRopository extends JpaRepository<Person,Integer> {
+
 }
