757c588d5244498e340903512455a12e8d21b029
kh-chaiboub <k.chaiboub@outlook.com>
2022-08-24 23:32:42 +0200

    Progect initializ

---
+++ src/test/java/com/gestion_pointage/Pointage/PointageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_pointage.Pointage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
